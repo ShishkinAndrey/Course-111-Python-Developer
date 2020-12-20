be8a54bcf50d6f16a655bc5a681cefe3ce917eae
ShishkinAndrey <shishkin1994@yandex.ru>
2020-12-20 20:51:03 +0300

    add def check_brackets

--- Tasks/a3_check_brackets.py
+++ Tasks/a3_check_brackets.py
@@ -5,5 +5,26 @@ def check_brackets(brackets_row: str) -> bool:
     :param brackets_row: input string to be checked
     :return: True if valid, False otherwise
     """
+    op_counter = 0
+    cl_counter = 0
+    op_list = []
+    if brackets_row == '':
+        return True
+    for i in brackets_row:
+        if i == '(':
+            op_counter += 1
+            op_list.append(i)
+        elif i == ')':
+            cl_counter += 1
+            if len(op_list)>0:
+                op_list.pop()
+            else:
+                return False
+    if op_counter != cl_counter:
+        return False
+    else:
+        return True
 
-    return False
+
+if __name__ == '__main__':
+    print(check_brackets("(((()))"))
