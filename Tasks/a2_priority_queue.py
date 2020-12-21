ef66952e4bad900c510d64391815f4dbe4236817
ShishkinAndrey <shishkin1994@yandex.ru>
2020-12-21 22:06:49 +0300

    changed the begining of the queue with priority. Now begining on the right side

--- Tasks/a2_priority_queue.py
+++ Tasks/a2_priority_queue.py
@@ -15,8 +15,8 @@ def enqueue(elem: Any, priority: int = 0) -> None:
     :param elem: element to be added
     :return: Nothing
     """
-    items_list.append((elem, priority))
-    items_list.sort(key=lambda x: x[1])
+    items_list.insert(0, (elem, priority))
+    items_list.sort(key=lambda x: x[1], reverse=True)
     queue_priority.clear()
     for i in items_list:
         queue_priority.append(i[0])
@@ -29,8 +29,8 @@ def dequeue() -> Any:
 
     :return: dequeued element
     """
-    items_list.pop(0)
-    return None if len(queue_priority) == 0 else queue_priority.pop(0)
+    items_list.pop(-1)
+    return None if len(queue_priority) == 0 else queue_priority.pop(-1)
 
 
 def peek(ind: int = 0, priority: int = 0) -> Any:
@@ -40,7 +40,7 @@ def peek(ind: int = 0, priority: int = 0) -> Any:
     :param ind: index of element (count from the beginning)
     :return: peeked element
     """
-    return None if ind >= len(queue_priority) else queue_priority[ind]
+    return None if ind >= len(queue_priority) else queue_priority[-1-ind]
 
 
 def clear() -> None:
@@ -53,15 +53,26 @@ def clear() -> None:
     return None
 
 if __name__ == '__main__':
-    enqueue(10,0)
-    enqueue(0,10)
+    enqueue(10, 0)
+    enqueue(0, 10)
+
     print(queue_priority)
     print(items_list)
     dequeue()
     print('-'*10)
-    print(queue_priority)
     print(items_list)
-    # enqueue(1, 10)
+    print(queue_priority)
+    enqueue(1, 10)
+    enqueue(6, 5)
+    print('-' * 10)
+    print(items_list)
+    print(queue_priority)
+    dequeue()
+    print(items_list)
+    print(queue_priority)
+
+
+    #
     # enqueue(5, 5)
 
 
