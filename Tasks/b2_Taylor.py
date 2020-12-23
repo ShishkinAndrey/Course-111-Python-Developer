4729d9c9cd054e59c93ee91a29ae17b07f174cac
ShishkinAndrey <shishkin1994@yandex.ru>
2020-12-23 18:31:54 +0300

    add sin(x). Need to do corrections with accurate

--- Tasks/b2_Taylor.py
+++ Tasks/b2_Taylor.py
@@ -2,7 +2,9 @@
 Taylor series
 """
 from typing import Union
-
+from itertools import count
+from math import factorial
+DELTA = 0.0001
 
 def ex(x: Union[int, float]) -> float:
     """
@@ -22,5 +24,13 @@ def sinx(x: Union[int, float]) -> float:
     :param x: x value
     :return: sin(x) value
     """
-    print(x)
-    return 0
+    def item(n):
+        '''Подсчет очередного элемента бесконечного ряда Тейлора для sin(x)'''
+        return ((-1) ** (n)) * ((x**(2*n + 1))/factorial(2*n + 1))
+    sum_ = 0
+    for i in count(start=0):
+        sum_ += item(i)
+        current_value = item(i)
+        if current_value < DELTA:
+            return sum_
+
