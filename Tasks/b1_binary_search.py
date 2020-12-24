62153e6592b3564a437f2ee836759019173d6db9
ShishkinAndrey <shishkin1994@yandex.ru>
2020-12-24 20:57:07 +0300

    add binary search

--- Tasks/b1_binary_search.py
+++ Tasks/b1_binary_search.py
@@ -11,35 +11,22 @@ def binary_search(elem: int, arr: Sequence) -> Optional[int]:
     """
     if not arr:
         return None
-    left_border = 0
-    right_border = len(arr) - 1
-    current_index = 0
-    i = 0
-    print(arr)
+    left = 0
+    right = len(arr) - 1
+    current_index = (left + right) // 2
     while True:
-        if left_border > right_border:
-            return None
-        if arr[left_border] == elem:
-
-            return left_border
-        if arr[right_border] == elem:
-            while arr[right_border-1] == elem:
-                right_border -= 1
-            return right_border
-        current_index = (left_border + right_border) // 2
-        print(f'{i}: current index - {current_index} ')
-        print(f'{i}: left - {left_border} ')
-        print(f'{i}: right - {right_border} ')
-        if elem == arr[current_index]:
-            return arr.index(elem)
-        elif elem < arr[current_index]:
-            right_border = current_index - 1
+        if elem < arr[current_index]:
+            right = current_index - 1
         elif elem > arr[current_index]:
-            left_border = current_index + 1
-        i += 1
-
-
+            left = current_index + 1
+        else:
+            while arr[current_index-1] == elem:
+                current_index -= 1
+            return current_index
+        current_index = (left + right) // 2
+        if left > right:
+            return None
 
 
 if __name__ == '__main__':
-    print(binary_search(2, [1, 2, 2, 2]))
+    print(binary_search(2, [1, 2, 2, 2, 2, 2]))
