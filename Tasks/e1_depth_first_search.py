37cefcefc29831634c90ac0b240d319af7ace35d
ShishkinAndrey <shishkin1994@yandex.ru>
2021-01-11 23:22:28 +0300

    Поиск в глубину

--- Tasks/e1_depth_first_search.py
+++ Tasks/e1_depth_first_search.py
@@ -1,4 +1,6 @@
 from typing import Hashable, List
+from collections import deque
+
 import networkx as nx
 
 
@@ -10,5 +12,32 @@ def dfs(g: nx.Graph, start_node: Hashable) -> List[Hashable]:
     :param start_node: starting node of search
     :return: list of nodes in the visited order
     """
-    print(g, start_node)
-    return list(g.nodes)
+    visited = {node: False for node in g.nodes}  # словарь непосещенных вершин
+    d = deque()  # стек с неподожжеными вершинами
+    path = [start_node] # пройденный путь
+    visited[start_node] = True
+    for neighbor in g[start_node]:
+        d.append(neighbor)
+    while d:
+        print(d)
+        print(visited)
+        current_node = d.pop()
+        visited[current_node] = True
+        path.append(current_node)
+        for neighbor in g[current_node]:
+            if visited[neighbor] == False and neighbor not in d:
+                d.append(neighbor)
+    return path
+
+if __name__ == '__main__':
+    graph = nx.Graph()
+    graph.add_nodes_from("ABCDEFG")
+    graph.add_edges_from([
+        ('A', 'B'),
+        ('A', 'C'),
+        ('B', 'D'),
+        ('B', 'E'),
+        ('C', 'F'),
+        ('E', 'G'),
+    ])
+    print(dfs(graph, 'A'))
