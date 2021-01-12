300376767e988b1e56c5178aec151fd8cb960916
ShishkinAndrey <shishkin1994@yandex.ru>
2021-01-12 21:22:31 +0300

    Реализована быстрая сортировка

--- Tasks/g2_quick_sort.py
+++ Tasks/g2_quick_sort.py
@@ -1,5 +1,5 @@
 from typing import List
-
+import random
 
 def sort(container: List[int]) -> List[int]:
     """
@@ -8,4 +8,14 @@ def sort(container: List[int]) -> List[int]:
     :param container: container of elements to be sorted
     :return: container sorted in ascending order
     """
-    return container
+    if len(container) < 2:
+        return container
+    else:
+        point = container[0]
+        less = [i for i in container[1:] if i <= point]
+        bigger = [i for i in container[1:] if i > point]
+    return sort(less) + [point] + sort(bigger)
+
+if __name__ == '__main__':
+    arr = [random.randint(0, 100) for _ in range(10)]
+    print(sort(arr))
