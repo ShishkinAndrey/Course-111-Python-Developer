176e478512e827824a0d205a24ed0cfbb54ba928
ShishkinAndrey <shishkin1994@yandex.ru>
2021-01-13 21:51:28 +0300

    Быстрая сортировка.Необходимы корректировки

--- Tasks/g2_quick_sort.py
+++ Tasks/g2_quick_sort.py
@@ -1,6 +1,22 @@
 from typing import List
 import random
 
+# способ номер 1
+# def sort(container: List[int]) -> List[int]:
+#     """
+#     Sort input container with quick sort
+#
+#     :param container: container of elements to be sorted
+#     :return: container sorted in ascending order
+#     """
+#     if len(container) < 2:
+#         return container
+#     else:
+#         point = container[0]
+#         less = [i for i in container[1:] if i <= point]
+#         bigger = [i for i in container[1:] if i > point]
+#     return sort(less) + [point] + sort(bigger)
+
 def sort(container: List[int]) -> List[int]:
     """
     Sort input container with quick sort
