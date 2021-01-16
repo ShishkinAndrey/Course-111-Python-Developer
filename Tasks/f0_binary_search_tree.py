e59243dbfffd24d09bfa3072f5be01d08fa62a44
ShishkinAndrey <shishkin1994@yandex.ru>
2021-01-16 18:30:35 +0300

    Выполнены insert, remove, clear

--- Tasks/f0_binary_search_tree.py
+++ Tasks/f0_binary_search_tree.py
@@ -15,29 +15,26 @@ def insert(key: int, value: Any) -> None:
     :param value: value associated with key
     :return: None
     """
-#     def _insert(bin_tree: dict):
-#         if not bin_tree:
-#             bin_tree = {
-#                 'key' == key,
-#                 'value' == value,
-#                 'left' == {},
-#                 'right' == {}
-#             }
-#         else:
-#             new_bin_tree = bin_tree['left'] if key <= bin_tree['key'] else bin_tree['right']
-#             _insert(new_bin_tree)
-#     _insert(bst)
-
-    def insert_(tree):
-        if tree is None:
-            tree = {'key': key, 'left': {}, 'right': {}}
-        elif key <= tree['key']:
-            btree= tree['left']
+    def _insert(root: dict, node: int):
+        # Сравнивает ключи и вставляет node
+        if not root:
+            root['key'] = node
+            root['value'] = value
+        elif node < root['key']:
+            if 'left' not in root.keys():
+                root['left'] = {}
+                root['left']['key'] = node
+                root['left']['value'] = value
+            else:
+                _insert(root['left'], node)
         else:
-            btree = tree['right']
-        insert_(btree)
-
-    insert_(bst)
+            if 'right' not in root.keys():
+                root['right'] = {}
+                root['right']['key'] = node
+                root['right']['value'] = value
+            else:
+                _insert(root['right'], node)
+    _insert(bst, key)
 
 def remove(key: int) -> Optional[Tuple[int, Any]]:
     """
@@ -46,8 +43,17 @@ def remove(key: int) -> Optional[Tuple[int, Any]]:
     :param key: key to be removed
     :return: deleted (key, value) pair or None
     """
-    print(key)
-    return None
+    def _delete(root: dict, target: int):
+        # Сравнивает ключи и находим target
+        if target == root['key']:
+            return (root.pop('key'), root.pop('value'))
+        elif root['key'] < target:
+            if 'right' in root.keys():
+                return _delete(root['right'], target)
+        elif root['key'] > target:
+            if 'left' in root.keys():
+                return _delete(root['left'], target)
+    return _delete(bst, key)
 
 
 def find(key: int) -> Optional[Any]:
@@ -57,8 +63,21 @@ def find(key: int) -> Optional[Any]:
     :param key: key for search in the BST
     :return: value associated with the corresponding key
     """
-    print(key)
-    return None
+    def _find(root: dict, target:int):
+        # Сравнивает ключи и находим target
+        if target == root['key']:
+            return root['value']
+        elif root['key'] < target:
+            if 'right' in root.keys():
+                return _find(root['right'],target)
+            else:
+                raise KeyError('Search of non-existing key should be erroneous.')
+        elif root['key'] > target:
+            if 'left' in root.keys():
+                return _find(root['left'],target)
+            else:
+                raise KeyError('Search of non-existing key should be erroneous.')
+    return _find(bst, key)
 
 
 def clear() -> None:
@@ -67,4 +86,18 @@ def clear() -> None:
 
     :return: None
     """
+    bst.clear()
     return None
+
+if __name__ == '__main__':
+    insert(0,2)
+    insert(1, 12)
+    insert(2, 323)
+    insert(11, 324)
+    insert(-3, 325)
+    print(bst)
+    print(find(11))
+    # print(remove(-3))
+    # clear()
+
+    print(bst)
