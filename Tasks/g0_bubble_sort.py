4e78660db61010179de5f96cbef6305ec8a8e831
ShishkinAndrey <shishkin1994@yandex.ru>
2021-01-20 21:51:32 +0300

    Внесены корректировки

--- Tasks/g0_bubble_sort.py
+++ Tasks/g0_bubble_sort.py
@@ -11,14 +11,12 @@ def sort(container: List[int]) -> List[int]:
     correct_compare = True
     while correct_compare:
         correct_compare = False
-        for i in range(len(container)):
-            if i == len(container)-1:
-                continue
+        for i in range(len(container)-1):
             if container[i] > container[i + 1]:
                 container[i], container[i+1] = container[i+1], container[i]
                 correct_compare = True
     return container
 
 if __name__ == '__main__':
-    lst = [14, 3, 17, 12]
+    lst = [14, 3, 17, 12,1,22,5]
     print(sort(lst))
