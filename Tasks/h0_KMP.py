7dc062033a63499c11aea2ff946fb5b7a0cb4001
ShishkinAndrey <shishkin1994@yandex.ru>
2021-01-20 19:24:11 +0300

    Выполнен поиск подстроки

--- Tasks/h0_KMP.py
+++ Tasks/h0_KMP.py
@@ -5,11 +5,24 @@ def _prefix_fun(prefix_str: str) -> List[int]:
     """
     Prefix function for KMP
 
-    :param prefix_str: dubstring for prefix function
+    :param prefix_str: substring for prefix function
     :return: prefix values table
     """
-    print(prefix_str)
-    return []
+    pi = [0] * len(prefix_str)  # префикс таблица
+    j = 0  # граница префикса
+    i = 1  # граница суффикса
+    while i < len(prefix_str):
+        if prefix_str[i] == prefix_str[j]:
+            pi[i] = j + 1
+            i += 1
+            j += 1
+        else:
+            if j == 0:
+                pi[i] = 0
+                i += 1
+            else:
+                j = pi[j - 1]
+    return pi
 
 
 def kmp_algo(inp_string: str, substr: str) -> Optional[int]:
@@ -20,6 +33,20 @@ def kmp_algo(inp_string: str, substr: str) -> Optional[int]:
     :param substr: substr to be found in inp_string (needle)
     :return: index where first occurrence of substr in inp_string started or None if not found
     """
-
-    print(inp_string, substr, _prefix_fun)
+    i = 0
+    j = 0
+    while i != len(inp_string):
+        if inp_string[i] == substr[j]:
+            i += 1
+            j += 1
+            if j == len(substr):
+                return i-j
+        else:
+            i += 1
+            j = _prefix_fun(substr)[j-1] if j != 0 else 0
     return None
+
+
+if __name__ == '__main__':
+    print(_prefix_fun('lo'))
+    print(kmp_algo("elfo", "fo"))
